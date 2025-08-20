c05bc8589bf877b641063546d40ccc7a9f1b4aef
Rinor Gllareva <93524834+RinorGllareva@users.noreply.github.com>
2025-08-20 19:38:26 +0200

    Home route

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/Home" element={<HomePage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
